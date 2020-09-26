2158fb5a6a1038e0124cb13e6a5415fdbf2e29f1
C0lliNN <rapphaelmanhaes2017@hotmail.com>
2020-09-26 16:44:18 -0300

    Updates

---
+++ precache-manifest.8431621f2c5d4195daf8422dd6791f6c.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b22a84f545a2f894493126b7410ec309",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70fffcf5f25340b77f3a",
+    "url": "/static/css/main.0e1bb267.chunk.css"
+  },
+  {
+    "revision": "06fc40d3d7e8da74f98c",
+    "url": "/static/js/2.f9e9c968.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f9e9c968.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70fffcf5f25340b77f3a",
+    "url": "/static/js/main.b44deff3.chunk.js"
+  },
+  {
+    "revision": "4a42fdcbdfd3cf55fea3",
+    "url": "/static/js/runtime-main.1c47a842.js"
+  },
+  {
+    "revision": "4772f63d231af11d1efae80668297590",
+    "url": "/static/media/arrow-left.4772f63d.svg"
+  },
+  {
+    "revision": "e2801e059100742c4e1d692feec0c3e4",
+    "url": "/static/media/arrow-right.e2801e05.svg"
+  },
+  {
+    "revision": "8757e7b687fdc0398255538e55c743ec",
+    "url": "/static/media/email.8757e7b6.svg"
+  },
+  {
+    "revision": "e2ad7b617d89ec6d495ddc939b4efda3",
+    "url": "/static/media/facebook.e2ad7b61.svg"
+  },
+  {
+    "revision": "ec38e187ec47b213a3ca22d15cc59af7",
+    "url": "/static/media/hero-banner__image.ec38e187.png"
+  },
+  {
+    "revision": "f40a301da68adc6f7c5ac6d6e54fe25d",
+    "url": "/static/media/instagram.f40a301d.svg"
+  },
+  {
+    "revision": "4217fddac471f30f8fbf7d0c70664143",
+    "url": "/static/media/logo-yooh.4217fdda.svg"
+  },
+  {
+    "revision": "86110fe1841dbc5cb978c009d04a09af",
+    "url": "/static/media/logo.86110fe1.svg"
+  },
+  {
+    "revision": "3305ca309a216daaf07696d07367a952",
+    "url": "/static/media/menu.3305ca30.svg"
+  },
+  {
+    "revision": "39c711f3de482116bb8980552f12c7de",
+    "url": "/static/media/pikachu.39c711f3.png"
+  },
+  {
+    "revision": "4ff8b8b07c30b2a844fa450e01c39b29",
+    "url": "/static/media/pokerbola.4ff8b8b0.png"
+  },
+  {
+    "revision": "fb0d701dde12b0f10717711dd0f84349",
+    "url": "/static/media/youtube.fb0d701d.svg"
+  }
+]);
